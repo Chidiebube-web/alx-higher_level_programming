9de8e3d7a919ff3b47dbcc750afd2ebdc9c9c44e
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-01 01:20:48 +0100

    file updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if l != 101 or l != 113:
-        print("{:c}".format(l), end='')
+for sm in range(97, 123):
+    if sm != 101 and sm != 113:
+        print("{:c}".format(sm), end='')
