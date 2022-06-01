66a2bf52d32befe6926640cb3bcf56fcece7c325
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-01 11:13:51 +0000

    file updated

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for int in range(0, 9):
+    for rem in range(int + 1, 10):
+        if int == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j))
