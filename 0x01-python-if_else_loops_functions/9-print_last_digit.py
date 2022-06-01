11171e31018bca4709ef4cd11ed1c35419e61e3e
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-02 00:29:14 +0100

    correction updated

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,4 @@ def print_last_digit(number):
     if number >= 0:
         print("{:d}".format(number % 10))
     else:
-        print("{:d}".format(number % 10))
+        print("{:d}".format(number % -10))
