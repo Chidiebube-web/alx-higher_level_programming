602405819702cda75e17f91e601f0a922d5ca3ff
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-01 11:17:34 +0000

    corrections updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for int in range(0, 9):
     for rem in range(int + 1, 10):
         if int == 8:
-            print("{}{}".format(i, j))
+            print("{}{}".format(int, rem))
         else:
-            print("{}{}".format(i, j))
+            print("{}{}".format(int, rem))
