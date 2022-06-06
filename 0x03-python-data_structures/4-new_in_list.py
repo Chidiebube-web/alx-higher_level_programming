970c7d08f291212f25a0c13b44bf37aaf3ece207
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-06 23:42:08 +0000

    bugs fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     cop = my_list.copy()
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return cop
     else:
         cop[idx] = element
