eb41b64cd7d4f708027f5a4b9e1add0425e4c786
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-06 22:32:26 +0000

    bugs fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx > range(my_list) - 1:
-        return None
+        return 'None'
     else:
-        my_list[idx]
+        return my_list[idx]
 
