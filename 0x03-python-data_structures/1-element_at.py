161c41761e0d625e36c740118b536e127f8eae4c
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-06 22:22:56 +0000

    coreections updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def element_at(mY_list, idx):
+def element_at(my_list, idx):
     for i in range(my_list):
         if idx < 0 or idx > range(my_list):
             return None
         else:
-             my_list[i] = my_list[idx]
+             idx = my_list[i]
+
