823835867e70e0cbcb909f8f9d3fe47a6f9cdd21
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-06 22:19:17 +0000

    file executable

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(mY_list, idx):
+    for i in range(my_list):
+        if idx < 0 or idx > range(my_list):
+            return None
+        else:
+            idx = my_list[i]
