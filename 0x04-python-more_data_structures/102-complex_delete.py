8cee443498cee9347159dd50d7f610a092c60bb2
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-09 11:23:29 +0000

    bug fixed

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def complete_delete(a_dictionary, value):
+def complex_delete(a_dictionary, value):
     for i in list(a_dictionary):
         if a_dictionary[i] == value:
             del(a_dictionary[i])
