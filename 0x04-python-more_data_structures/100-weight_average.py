ca9ee6c9f39428455baa22af9a8d7b49a5ec0b1b
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-09 10:39:07 +0000

    fiule executable

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    average = 0
+    other = 0
+    for i in my_list:
+        average += tup[0] * tup[1]
+        other += tup[1]
+    return float(average / other)
