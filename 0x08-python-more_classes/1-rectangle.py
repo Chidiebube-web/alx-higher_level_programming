9fb33dc5bdee3a42e48d8fae648a0b24aadeb1ae
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-06-27 18:17:02 +0000

    corrections updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,23 +13,23 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             """Returns a typeerror if NaN"""
-            return "TypeError: width must be an integer"
+            raise TypeError("width must be an integer")
         elif value < 0:
             """Returns a valueerror if value is less than 0"""
-            return "ValueError: width must be >= 0"
+            raise ValueError("width must be >= 0")
         else:
             """Sets the width value"""
-            self.value = value
+            self.width = value
     def height(self):
         """Returns the value of the height"""
         return self.__height
     def height(self, value):
         if type(value) != int:
             """returns a typeerror if value is NaN"""
-            return "TypeError: height must be an integer"
+            raise TypeError("height must be an integer")
         elif value < 0:
             """returns a valueerror if value is less than 0"""
-            return "ValueError: height must be >= 0"
+            raise ValueError("height must be >= 0")
         else:
             """sets the height value"""
-            self.value = value
+            self.height = value
