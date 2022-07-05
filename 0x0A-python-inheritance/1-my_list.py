37908be8215b8a456d00fcfdf2614b8551ac0ae0
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-07-05 22:05:46 +0000

    module documented

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,8 +7,10 @@ a super class 'list'
 
 class MyList(list):
     """Inherits from list class"""
+
     def print_sorted(self):
         """Prints a sorted list"""
+
         n_list = self[:]
         n_list.sort()
         print("{}".format(n_list)
