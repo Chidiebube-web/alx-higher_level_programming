661a97db86e7e615dde2b1f8d96ddd1552644616
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-07-06 22:50:00 +0000

    file updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,9 @@ Reads a text file"""
 def read_file(filename=''):
     """This function reads a text file
     in the UTF-8 encoding format and 
-    prints it to stdout """
+    prints it to stdout 
+    """
+
     with open(filename) as text_file:
         data = text_file.read()
         print(data, end="")
