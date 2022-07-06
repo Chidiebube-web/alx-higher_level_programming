fbe9080371acd41fca6ce4b135fef6915dda559c
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-07-06 22:45:36 +0000

    file made executable

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 0-read_file
+Reads a text file"""
+
+
+def read_file(filename=''):
+    """This function reads a text file
+    in the UTF-8 encoding format and 
+    prints it to stdout """
+    with open('filename', encoding='utf-8') as text_file:
+        data = text_file.read()
+        print(data, end="")
