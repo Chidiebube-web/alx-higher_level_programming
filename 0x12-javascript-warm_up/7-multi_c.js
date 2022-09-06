b0afeb0a54657bd7c50d24b67e9e62c7ad959b5e
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-09-06 11:40:05 +0000

    updated for checker

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	let word = 'C is fun';
-	for (let i = 0; i < process.argv[2]; i++) {
-		console.log(word);
-	}
+  const word = 'C is fun';
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(word);
+  }
 }
