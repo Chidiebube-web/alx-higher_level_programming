7eff24d3c7ac61157192982a8c4cf9bc40fc5d75
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-09-08 10:10:32 +0000

    updated for checker

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h){
+  constructor (w, h) {
     if(((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
       this.height = h;
       this.width = w;
